19324f2a0d16cd1f991977d73416f132e84fe677
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-02-04 12:05:34 +0530

    Add files via upload

---
+++ QUES4.C
@@ -0,0 +1,5 @@
+/*Question:
+Write a C program to find and print Ramanujan Numbers (Taxicab Numbers) up to a given limit.
+
+A Ramanujan number is a number that can be expressed as the sum of two cubes in more than one way:
+*/
