214fd773507d200ecd4b32da48e5f567c78dc2b9
Siddharth Sharma <siddharthshrm680@gmail.com>
2026-02-13 08:30:32 +0530

    Check wether student is pass or fail

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int marks;
+    printf("enter marks ");
+    scanf("%d",&marks);
+    if(marks>40)
+    {
+        printf("pass");
+
+    }
+    else 
+    {
+        printf("fail");
+    }
+    return 0;
+}
